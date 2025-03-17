b3b6bc1cff5cb33a035f8018dcd3f01d3b34dceb
VenukaSiriwardena <venukaransindusiriwardena@gmail.com>
2025-03-18 02:09:58 +0530

    routes group added

--- Backend-API/GameStore.Api/Program.cs
+++ Backend-API/GameStore.Api/Program.cs
@@ -33,10 +33,12 @@ List<Game> games = new()
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/games", () => games);
+var group = app.MapGroup("/games");
+
+group.MapGet("/", () => games);
 
 
-app.MapGet("/games/{id}", (int id) => 
+group.MapGet("/{id}", (int id) => 
 {
     Game? game = games.Find(game => game.Id == id);
 
@@ -49,7 +51,7 @@ app.MapGet("/games/{id}", (int id) =>
 .WithName(GetGameEndpointName);
 
 
-app.MapPost("/games", (Game game) =>
+group.MapPost("/games", (Game game) =>
 { 
     game.Id = games.Max(game => game.Id) + 1;
     games.Add(game);
@@ -57,7 +59,7 @@ app.MapPost("/games", (Game game) =>
     return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id}, game);
 });
 
-app.MapPut("/games/{id}", (int id, Game updatedGame) =>
+group.MapPut("/{id}", (int id, Game updatedGame) =>
 {
     Game? existingGame = games.Find(game => game.Id == id);
 
@@ -75,7 +77,7 @@ app.MapPut("/games/{id}", (int id, Game updatedGame) =>
 
 });
 
-app.MapDelete("/games/{id}", (int id) =>
+group.MapDelete("/{id}", (int id) =>
 {
     Game? game = games.Find(game => game.Id == id);
 
--- Backend-API/GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ Backend-API/GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43e8d73f381b37b929ab83b77de53cabf13a611a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2071e158497d6db24c0db61c5b6893eebf38117")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
