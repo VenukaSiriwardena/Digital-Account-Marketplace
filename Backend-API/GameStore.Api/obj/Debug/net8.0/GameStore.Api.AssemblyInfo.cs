a24539354524088cf865be42028f6189560592b4
VenukaSiriwardena <venukaransindusiriwardena@gmail.com>
2025-03-21 02:32:32 +0530

    added credentials

--- Backend-API/GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ Backend-API/GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55fbea8c1cfd7011cdba73c9ca809ac7a62abc95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+650e8834ee6120ef17bfbc88f1e08fd011c72180")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
