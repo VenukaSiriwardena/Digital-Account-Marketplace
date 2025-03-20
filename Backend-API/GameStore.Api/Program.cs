eac16599182259594bd962e5ccd0cdbe49f2b21b
VenukaSiriwardena <venukaransindusiriwardena@gmail.com>
2025-03-21 02:12:41 +0530

    database connection changed

--- Backend-API/GameStore.Api/Program.cs
+++ Backend-API/GameStore.Api/Program.cs
@@ -4,12 +4,27 @@ using GameStore.Api.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Fetch connection string
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Add DB context with SQL Server
+builder.Services.AddDbContext<GameStoreContext>(options =>
+    options.UseSqlServer(connectionString));
+
 builder.Services.AddRepositories(builder.Configuration);
 
 var app = builder.Build();
 
+// Ensure database migrations are applied
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+    await dbContext.Database.MigrateAsync(); // Applies pending migrations
+}
+
+// Initialize database
 await app.Services.InitializeDbAsync();
 
 app.MapGamesEndpoints();
-
 app.Run();
--- Backend-API/GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ Backend-API/GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc22baab3117042e3a5b746eab1872cfc9fea634")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55fbea8c1cfd7011cdba73c9ca809ac7a62abc95")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
